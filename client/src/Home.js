c6ac20c0eb01e30d83207d4f33708cfedf534ed8
phatTchung <phat.chung@bhcc.edu>
2026-05-26 12:05:25 -0400

    update - homepage

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,23 +1,62 @@
 import React from 'react'
+import { CgProfile } from "react-icons/cg";
+import { IoIosNotifications } from "react-icons/io";
 
-const Home = ({username}) => {
+const Home = ({ username }) => {
   return (
     <main className='homepage'>
-        <a href='#'>Applications</a>
-        <a href='#'>New hires</a>
-        <a href='#'>Attendance</a>
-        <a href='#'>Requests</a>
-        <a href='/departments'>Departments</a>
-        <a href='#'>Payroll</a>
-      {/* <section className='profile'>
-          <div>
-            <h1>{`Wellcome back, ${username}`}</h1>
+      <section className='profile'>
+        <div>
+          <h3>{`Wellcome back, ${username}`}</h3>
+        </div>
+        <div>
+          <button>
+            <IoIosNotifications />
+          </button>
+          <button>
+            <CgProfile />
+          </button>
+        </div>
+      </section>
+      <section className='dashboardContent'>
+        <div className='cards'>
+          <div className="card">
+            <h3>Applications</h3>
+            <p>Review job applications</p>
+            <a href="#">Open</a>
           </div>
-          <div>
-              <button></button>
-              <button></button>
+
+          <div className="card">
+            <h3>New Hires</h3>
+            <p>Manage onboarding</p>
+            <a href="#">Open</a>
           </div>
-      </section> */}
+
+          <div className="card">
+            <h3>Attendance</h3>
+            <p>Track employee attendance</p>
+            <a href="#">Open</a>
+          </div>
+
+          <div className="card">
+            <h3>Requests</h3>
+            <p>Approve employee requests</p>
+            <a href="#">Open</a>
+          </div>
+
+          <div className="card">
+            <h3>Departments</h3>
+            <p>View company departments</p>
+            <a href="/departments">Open</a>
+          </div>
+
+          <div className="card">
+            <h3>Payroll</h3>
+            <p>Manage salary and payment</p>
+            <a href="#">Open</a>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
