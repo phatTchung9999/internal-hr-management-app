3ee41ff2375dc35e31a8193694bfdcd8d3b08244
phatTchung9999 <chyngtanphat@gmail.com>
2026-06-06 15:50:21 -0400

    add allowed origin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
     'https://myhrmanager.azurewebsites.net',
-    'https://kind-mushroom-0d8d60b0f.7.azurestaticapps.net'
+    'https://kind-mushroom-0d8d60b0f.7.azurestaticapps.net',
+    'https://hrmanager.phatchung.dev/'
 ]
 
 module.exports = allowedOrigins;
