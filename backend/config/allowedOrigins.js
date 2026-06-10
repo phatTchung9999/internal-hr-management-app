e22782a94f6ac55aa832b52dd1478aff5d4994f2
phatTchung9999 <chyngtanphat@gmail.com>
2026-06-09 23:42:56 -0400

    add allowed origin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [  
     'http://localhost:3500',
     'http://localhost:3000',
+    'http://10.0.0.162:3000',
     'https://myhrmanager.azurewebsites.net',
     'https://kind-mushroom-0d8d60b0f.7.azurestaticapps.net',
     'https://hrmanager.phatchung.dev'
